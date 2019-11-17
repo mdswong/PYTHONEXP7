555c24896ff6b3f9a64b208d5fe7d15474ca511a
Marvin Wong <47411170+mdswong@users.noreply.github.com>
2019-11-17 21:19:19 +0800

    Update div_by_3.py

--- div_by_3.py
+++ div_by_3.py
@@ -1,3 +1,9 @@
+"""
+Create the following 10x10 ndarray, which is the squares of the first 100
+positive integers. From this ndarray, determine all the elements that are
+divisible by 3. Save the result as div_by_3.npy
+"""
+
 import numpy as np
 
 B = np.arange(1,101,1)
